9b1c87b19bd150fa9856dbdd898223da2c79027f
ardi <0x4rd1@gmail.com>
2025-01-09 06:57:49 +0700

    Remove indent-blankline, because duplicate to snacks nvim

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,5 +2,23 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  opts = {},
+  opts = {
+    indent = {
+      priority = 1,
+      enabled = true,       -- enable indent guides
+      char = "│",
+      only_scope = false,   -- only show indent guides of the scope
+      only_current = false, -- only show indent guides in the current window
+      hl = {
+        "SnacksIndent",
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      },
+    }
+  },
 }
